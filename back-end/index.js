060467488360791953d6dcb7c0c10b7ee8f3e1c3
skmax1 <keshav.shri06@gmail.com>
2024-04-28 17:43:24 +0530

    feat: implemented signup route

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 import { rootRouter } from "./routes";
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1",rootRouter);
+
+const PORT = 3000;
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running port ${PORT}`)
+});
 
 
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,6 +1,56 @@
 const express = require("express")
 const router = new express.Router();
 const app = express();
+const zod = require("zod");
+const mongoose = require("mongoose");
+const { User } = require("../db");
+const jwt  = require("jsonwebtoken");
+const { jwt_secret } = require("../config");
+
+const signupBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstname: zod.string(),
+    lastname: zod.string()
+});
+
+app.post("/signup", async (req,res)=>{
+    const {sucess} = signupBody.safeParse(req.body);
+    if(!sucess){
+        return res.status(411).json({
+            msg: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if(existingUser){
+        return res.status(411).json({
+            msg: "Email already taken"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname
+
+    })
+
+    const userid = user._id;
+
+    const token = jwt.sign({userid},jwt_secret);
+
+    res.json({
+        msg: "User created successfully"
+    })
+});
+
+
+
 
 module.exports={
     router
