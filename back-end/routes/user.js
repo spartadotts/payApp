7fa8a1930aaef24efb10901b65afdd80403b8326
skmax1 <keshav.shri06@gmail.com>
2024-04-29 16:09:42 +0530

    feat:added find other users route

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -109,6 +109,31 @@ router.put("/",authMiddleware,async (req,res)=>{
 
 })
 
+router.get("/bulk", async (req,res)=>{
+    const filter = req.query.filter || " ";
+
+    const users = await User.find({
+        $or:[{
+            firstname:{
+                "$regex":filter
+            }
+        },{
+            lastname:{
+                "$regex": filter
+            }
+        }]
+    })
+
+
+    res.json({
+        user: users.map(user=>({
+            username: user.username,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            id: user._id
+        }))
+    })
+})
 
 module.exports={
     router
